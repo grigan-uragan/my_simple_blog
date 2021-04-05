cbc1b84231d64d3af1059b694a2e8f0bd76e1179
grigan-Uragan <prischepny@mail.ru>
2021-04-06 00:08:39 +0300

    add admin page

---
+++ src/main/java/ru/grigan/my_blog/controller/UserController.java
@@ -0,0 +1,56 @@
+package ru.grigan.my_blog.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.grigan.my_blog.model.Role;
+import ru.grigan.my_blog.model.User;
+import ru.grigan.my_blog.repository.UserRepository;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/user")
+@PreAuthorize("hasAnyAuthority('ADMIN')")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public String allUsers(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String editUser(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String saveUser(@RequestParam String username,
+                           @RequestParam Map<String, String> form,
+                           @RequestParam("userId") User user) {
+        user.setUsername(username);
+        Set<String> roles =
+                Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+        return "redirect:/user";
+    }
+}
